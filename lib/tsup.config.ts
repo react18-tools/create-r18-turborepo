2aee9c07e9cd818cb67cce24a866d7a4242488e7
Mayank <mayank.srmu@gmail.com>
2025-08-22 20:41:20 +0530

    Add turborepo-template-init to canonical package publishing script

--- lib/tsup.config.ts
+++ lib/tsup.config.ts
@@ -1,23 +1,17 @@
 import { defineConfig, type Options } from "tsup";
-import react18Plugin from "esbuild-plugin-react18";
-import cssPlugin from "esbuild-plugin-react18-css";
 import { rdiPlugin } from "esbuild-plugin-rdi";
 
 export default defineConfig(
   (options: Options) =>
     ({
-      format: ["cjs", "esm"],
+      format: ["esm"],
       target: "es2019",
       entry: ["./src/**"],
       sourcemap: false,
       clean: !options.watch,
       bundle: true,
       minify: !options.watch,
-      esbuildPlugins: [
-        react18Plugin({ disableJSXRequireDedup: true }),
-        cssPlugin({ generateScopedName: "[folder]__[local]" }),
-        rdiPlugin(),
-      ],
+      esbuildPlugins: [rdiPlugin()],
       ...options,
     }) as Options,
 );
--- scripts/publish-canonical.js
+++ scripts/publish-canonical.js
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process");
 
 // Publish canonical packages
-["create-r18", "create-r18-turbo"].forEach(pkg => {
+["create-r18", "create-r18-turbo", "turborepo-template-init"].forEach(pkg => {
   execSync(`sed -i -e "s/name.*/name\\": \\"${pkg.replace(/\//g, "\\\\/")}\\",/" lib/package.json`);
   execSync("cd lib && npm publish --provenance --access public");
 });
